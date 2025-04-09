8902cbfc871bd41f2259b69bdcfe2c947957eb05
DevAgnihotri <agnihotrivibhasanjeevdev108@gmail.com>
2025-04-09 12:05:08 +0530

    updating urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -36,10 +36,12 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
+    #   Or simply just
+    #   question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    return HttpResponse("You're voting on quuestion %s" % question_id)
+    return HttpResponse("You're voting on question %s" % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
